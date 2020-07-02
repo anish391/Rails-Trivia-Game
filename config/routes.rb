067178b1690c4609418b2dfd8cbea972fd5a53b0
Anish Gadekar <argads@gmail.com>
2020-07-01 20:40:56 -0700

    Add layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  root  'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
