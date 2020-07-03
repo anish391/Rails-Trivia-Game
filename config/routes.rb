eb7a83a96c8da24a854646c038de8371609d1b87
Anish Gadekar <argads@gmail.com>
2020-07-03 14:28:20 -0700

    Remove unnecessary pages.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
 
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
